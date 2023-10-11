bb5ecbd0ef0b5c5bc5f777da621aae1cd483d8d4
RuangNolepHybrid <valhybridval@gmail.com>
2023-10-11 23:34:53 +0700

    push pertama kali

---
+++ test.js
@@ -0,0 +1,27 @@
+//perkenalan variables
+let a = "a"; // disarankan pakai let
+var b = "b"; //udah jarang dipakai
+const c = "c";
+
+//variabel bisa menampung tipe data string, integer, boolean, array, object
+let nama = "hybrid";
+let umur = 20;
+let apakahBenar = true;
+let uang = 5000.5;
+
+//operator aritmatika
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 * angka2);
+
+let result = (angka1 + angka2) * angka2;
+console.log("Hasil dari kerjaan kita: ", result);
+
+let number = 10;
+console.log(number);
+number = 20;
+console.log(number);
+number = false;
+console.log(number - 10);
+console.log(angka1 / angka2);
